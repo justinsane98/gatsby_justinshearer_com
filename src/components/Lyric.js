86c359fc6f9d57e16d53c1a53dd91b7e75ec0d2e
Justin Shearer <justin.shearer@gmail.com>
2021-10-18 09:26:19 -0700

    performance

--- src/components/Lyric.js
+++ src/components/Lyric.js
@@ -4,7 +4,7 @@ const Lyric = function (props) {
   return (
     <div
       style={{ textShadow: "2px 2px 0 rgba(0,0,0,0.1)" }}
-      className="lyric text-white-75 text-base 425px:text-base 768px:text-lg 1024px:text-xl font-normal font-serif leading-tight sm:leading-loose z-10 mb-4 w-3/4 375px:w-full"
+      className="lyric inline-block text-white-75 text-base 425px:text-base 768px:text-lg 1024px:text-xl font-normal font-serif leading-tight sm:leading-loose z-10 mb-4 w-3/4 375px:w-full"
     >
       {props.content}
     </div>
