93be75bde45c386148a8c44b39c328e03f344bcf
Justin Shearer <justin.shearer@gmail.com>
2023-09-01 16:48:18 -0400

    new firebase form v mailchimp legacy

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,21 @@
-import React from "react";
-import MailchimpSubscribe from "react-mailchimp-subscribe"
+import React, {Component } from "react";
 import { motion} from 'framer-motion'
 import data from '../data/data'
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAziM8Dkq3BwvskKCnEI-OGU7z4SKYwLAo",
+  authDomain: "justinshearerdotcom.firebaseapp.com",
+  projectId: "justinshearerdotcom",
+  storageBucket: "justinshearerdotcom.appspot.com",
+  messagingSenderId: "897571319748",
+  appId: "1:897571319748:web:6466e6e32fb2423650fe91",
+  measurementId: "G-QD6CG8C72D"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 const duration = 0.5
 const props = data.properties[1];
 const item = {
@@ -18,69 +31,71 @@ const item = {
   },
 };
 
-const formUrl = "https://justinshearer.us5.list-manage.com/subscribe/post?u=6632f26fcdf40d8319e2e9c9a&id=c1f3eb3ecc";
+class Form extends Component {
 
-const MailchimpForm = ({ status, message, onValidated }) => {
-    let email, note;
-    const submit = () =>
-      email &&
-      note &&
-      email.value.indexOf("@") > -1 &&
-      onValidated({
-        EMAIL: email.value,
-        NOTE: note.value
-      });
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: "init",
+    }
+  }
+
+  saveData = async () => {
+    this.setState({
+      status: "loading"
+    })
+    await addDoc(collection(db, "contacts"), {
+      email: this.state.email,
+      note: this.state.note,
+      created: new Date(Date.now()).toLocaleString()
+    }).then(() => {
+      this.setState({
+        status: "success"
+      })
+    });
+  }
+  updateNote = (note) => {
+    this.setState({
+      note: note
+    })
+  }
+
+  updateEmail = (email) => {
+    this.setState({
+      email: email
+    })
+  }
+
+  render() {
   
     return (
-      <div>
+
+        <div>
         <div className="mb-2 text-white bg-pink-50">
-            {status === "sending" && <p className="px-4 py-2">sending...</p>}
-            {status === "error" && (
-                <div
-                className="px-4 py-2 bg-pink"
-                dangerouslySetInnerHTML={{ __html: message }}
-                />
-            )}
-            {status === "success" && (
+            {this.state.status === "loading" && <p className="px-4 py-2">sending...</p>}
+            {this.state.status === "success" && (
                 <div
                 className="px-4 py-2"
-                >Thanks for the note! I'll reach out as soon as I'm done wrangling all of these cats. </div>
+                >Thanks for the note! I'll reach out as soon as I'm done wrangling cats.</div>
             )}
         </div>
 
          <motion.div key={"textarea"} variants={item}>
-            <textarea ref={node => (note = node)} className="font-serif text-xs 425px:text-base 768px:text-lg p-2 w-full h-32 shadow-default border-pink-dark border-2 focus:outline-none focus:border-pink bg-white-75 focus:bg-white"
-                    placeholder="How can I help you?"
-                    ></textarea>
-        </motion.div>
-        <motion.div key={"email"} variants={item}>
-            <input ref={node => (email = node)} className="font-serif text-xs 425px:text-base 768px:text-lg  p-2 w-full shadow-default border-pink-dark border-2 focus:outline-none focus:border-pink bg-white-75 focus:bg-white" placeholder="What's your Email?"></input>
-        </motion.div>
-        <motion.div key={"send"} variants={item}>
-            <button onClick={submit} className="font-display text-white text-xs 425px:text-base 768px:text-lg font-thin uppercase bg-pink py-2 px-4 mt-2 inline-block shadow-default border-pink border-2 text-white focus:bg-pink-dark active:bg-pink-light hover:bg-pink-light hover:text-white hover:cursor-pointer scale-100 hover:scale-110 transition-bg transition-scale duration-200 ease-in-out">{props.cta}</button>
-        </motion.div>
+            <textarea className="font-serif text-xs 425px:text-base 768px:text-lg p-2 w-full h-32 shadow-default border-pink-dark border-2 focus:outline-none focus:border-pink bg-white-75 focus:bg-white"
+                    onChange={e => this.updateNote(e.target.value)} placeholder="How can I help you?"
+            ></textarea>
+          </motion.div>
+          <motion.div key={"email"} variants={item}>
+              <input onChange={e => this.updateEmail(e.target.value)} className="font-serif text-xs 425px:text-base 768px:text-lg  p-2 w-full shadow-default border-pink-dark border-2 focus:outline-none focus:border-pink bg-white-75 focus:bg-white" placeholder="What's your Email?"></input>
+          </motion.div>
+          <motion.div key={"send"} variants={item}>
+              <button onClick={this.saveData} className="font-display text-white text-xs 425px:text-base 768px:text-lg font-thin uppercase bg-pink py-2 px-4 mt-2 inline-block shadow-default border-pink border-2 text-white focus:bg-pink-dark active:bg-pink-light hover:bg-pink-light hover:text-white hover:cursor-pointer scale-100 hover:scale-110 transition-bg transition-scale duration-200 ease-in-out">{props.cta}</button>
+          </motion.div>
 
-      </div>
-    );
-  };
-  
-  const Form = () => {
+        </div>
 
-      return (
+)
+}
+}
 
-          <MailchimpSubscribe
-            url={formUrl}
-            render={({ subscribe, status, message }) => (
-              <MailchimpForm
-                status={status}
-                message={message}
-                onValidated={formData => subscribe(formData)}
-              />
-            )}
-          />
-
-      );
-  }
-  
-
-  export default Form;
+export default Form
